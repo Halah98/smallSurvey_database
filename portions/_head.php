3eb6d7193c7570ad009193138b6f1c9a1c85be02
Halah98 <63377931+Halah98@users.noreply.github.com>
2021-07-06 18:09:42 +0300

    Add files via upload

---
+++ portions/_head.php
@@ -0,0 +1,17 @@
+<head>
+<title> Survey </title>
+		<meta charset="utf-8" />
+		<meta name="description" content="Survey" />
+		<meta name="keywords" content="Survey" />
+		
+    <!-- Bootstrap Css link -->
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" 
+        integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+         
+    <!-- JQuery JS-->
+       <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous"></script> 
+
+      <link rel="stylesheet" href="style.css">
+
+</head>
